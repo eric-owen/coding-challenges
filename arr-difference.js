2710340b31cbcd2f7f4392237b607dbd1a0a8d95
eric-owen <65836532+eric-owen@users.noreply.github.com>
2020-09-26 18:44:27 -0400

    added new challenge- array differences

---
+++ arr-difference.js
@@ -0,0 +1,31 @@
+/* Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b.*/
+
+const a = [3, 4]
+const b = [3]
+let newArr = []
+
+
+// without filter
+function arrayDiff(a, b) {
+    for (i = 0; i < a.length; i++) {
+        if (!b.includes(a[i])) {
+            newArr.push(a[i])
+        }
+    }
+    return newArr
+}
+
+// with filter 
+function arrayDiffver2(a, b) {
+    return a.filter(function (cur) {
+        return !b.includes(cur)
+    })
+}
+
+
+console.log(arrayDiff(a, b))
+console.log(arrayDiffver2(a, b))
+
+
